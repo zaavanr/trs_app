b756457b854d2a2237c76bbbf3524190c1a29d7f
Alton Bodley <altonbodley@gmail.com>
2017-02-07 16:33:32 -0500

    route drawing v1

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -5,20 +5,25 @@
 
 // ------Global Variables-----
 var map; // The map object
-var pickUp; //pick-up location
+var pickUp; //pick-up location Marker
 var cpos; //current (position) location of device
-var dest ={lat:18.017213,
-            lng: -76.758697}; //destination
-var cloc; //Client location
+var dest; //destination Marker
+var destLoc;//destination location
+var pickUpLoc; //Pick up location
 var pos;
-
+var coords;
+var geocoder;
 // -----Generate Map-----
 function createMap(){
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 18.0179, lng: -76.8199},
     zoom: 14
   });
+  geocoder = new google.maps.Geocoder();
   getCurrent();
+  setPickUp();
+  setDestination();
+  drawRoute(pickUpLoc,destLoc);
 }
 
 // --Returns the current location of the user--
@@ -30,15 +35,17 @@ function getCurrent() {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-    pos =new google.maps.Marker({
+    pickUp =new google.maps.Marker({
       position:cpos,
       title: 'Current Location',
       draggable:true
     });
+    pickUpLoc=cpos;
     map.setCenter(cpos);
-    pos.setMap(map);
-    console.log("current pos; lat:"+cpos.lat+" lng:"+cpos.lng)
-    geocodeAddress(map)
+    pickUp.setMap(map);
+    console.log("current pos; lat:"+cpos.lat+" lng:"+cpos.lng);
+    console.log("Pickup if set to current: "+pickUpLoc.lat+","+pickUpLoc.lng);
+    //geocodeAddress(map)
     // $.ajax('/save-coord', {
     //   data: {
     //     x:  '12',
@@ -56,55 +63,95 @@ function getCurrent() {
   }
 }
 
-// ----- Address search Listener---
-function addressSearch(){
-  var geocoder = new google.maps.Geocoder();
-  document.getElementById(/*'Your ID'*/).addEventListener('click',function(){
-    geocodeAddress(geocoder,map);
-  });
+function setPickUp(){
+  ///document.getElementById(/*'Your ID'*/).addEventListener('click',function(){
+    var address='656 Hope Rd Kingston';
+    geocoder.geocode({'address': address},function(results,status){
+      if (status === 'OK'){
+        pickUpLoc = results[0].geometry.location;
+      }else{('We could not locate the address you entered. Status: '+status);}
+  //});
+  console.log("Pick up Loc if search "+pickUpLoc);
+  pickUp.setPosition(pickUpLoc);
+  pickUp.setMap(map);
+  map.setCenter(pickUpLoc);
+});
 }
+function setDestination(){
+  ///document.getElementById(/*'Your ID'*/).addEventListener('click',function(){
+    var address='121 Old Hope Road, Kingston';
+    geocoder.geocode({'address': address},function(results,status){
+      if (status === 'OK'){
+        destLoc = results[0].geometry.location;
+      }else{('We could not locate the address you entered. Status: '+status);}
+  //});
+  console.log("Destination "+destLoc);
+  dest =new google.maps.Marker({
+    position:destLoc,
+    title: 'Destination',
+    draggable:false
+  });
+  dest.setMap(map);
+  map.setCenter(destLoc);
+});
+// drawRoute(pickUpLoc,destLoc);
+}
+
+// ----- Address search Listener---
+// function addressSearch(){
+//   //var geocoder = new google.maps.Geocoder();
+//   ///document.getElementById(/*'Your ID'*/).addEventListener('click',function(){
+//     var address='656 Hope Rd Kingston';
+//     geocodeAddress(address);
+//   //});
+// }
 
 /* -----Translates an Address into coodinates
  and places pick-up marker at that location---
  @param: geocoder:Geocoder object
         map: map object*/
-function geocodeAddress(map){
-  var geocoder = new google.maps.Geocoder();
-  var address= '656 Hope Rd Kingston';//document.getElementById('address').value;
-  geocoder.geocode({'address': address},function(results,status){
-    if (status === 'OK'){
-      map.setCenter(results[0].geometry.location);
-      pickUp =new google.maps.Marker({
-      position:results[0].geometry.location,
-      title: 'Current',
-      draggable:true
-      });
-      pos.setMap(null);
-      pickUp.setMap(map);
-      console.log("pickUp "+pickUp.position);
-    }else{
-      alert('We could not locate the address you entered. Status: '+status);
-    }
-  });
-}
+
+// function geocodeAddress(address){
+//   var geocoder = new google.maps.Geocoder();
+//   //var address= '656 Hope Rd Kingston';//document.getElementById('address').value;
+//   geocoder.geocode({'address': address},function(results,status){
+//     if (status === 'OK'){
+//       var coords= results[0].geometry.location;
+//       console.log ("coords "+coords);
+//       return coords;
+//       // map.setCenter(results[0].geometry.location);
+//       // pickUp =new google.maps.Marker({
+//       // position:results[0].geometry.location,
+//       // title: 'Pick up location',
+//       // draggable:true
+//       // });
+//       // pos.setMap(null);
+//       // pickUp.setMap(map);
+//       // console.log("pickUp "+pickUp.position);
+//     }else{
+//       alert('We could not locate the address you entered. Status: '+status);
+//     }
+//   });
+// }
 
 /*-----Draw route between two points-----
 @param: pickUp: pick up coordinates
         dest: destination coordinates*/
-function drawRoute(pickUp,dest){
+function drawRoute(pickUpLoc,destLoc){
   var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new gogle.maps.DirectionsRenderer;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
   directionsDisplay.setMap(map);
-  document.getElementById(/*Search/Request*/).addEventListener('click',function(){
+  //document.getElementById(/*Search/Request*/).addEventListener('click',function(){
     directionsService.route({
-    origin: document.getElementById(/*pickUp*/).value,
-    destination: document.getElementById(/*dest*/).value,
-    waypoints: cloc,
+    origin: '656 Hope Rd Kingston',//document.getElementById(/*pickUp*/).value,
+    destination: '121 Old Hope Road, Kingston',//document.getElementById(/*dest*/).value,
+    //waypoints: ['656 Hope Rd Kingston'],
     travelMode: 'DRIVING'
     },function(response,status){
         if(status=='OK'){
           directionsDisplay.setDirections(response);
+          //destLoc.setMap(null);
         }else{console.log("directions failed: "+status);}
-    });
-  });
+    });console.log("Route");
+  //});
 }
