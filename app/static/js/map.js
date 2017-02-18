710d12afd338f5568e04d265027ad20c641624c6
Alton Bodley <altonbodley@gmail.com>
2017-02-18 03:14:50 -0500

    Search and draw route functionality

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -13,17 +13,20 @@ var pickUpLoc; //Pick up location
 var pos;
 var coords;
 var geocoder;
+var markers=[pickUp,dest];
 // -----Generate Map-----
 function createMap(){
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 18.0179, lng: -76.8199},
-    zoom: 14
+    zoom: 14,
+    mapTypeId: 'roadmap'
   });
   geocoder = new google.maps.Geocoder();
   getCurrent();
+  search();
   setPickUp();
-  // setDestination();
-  // drawRoute(pickUpLoc,destLoc);
+  setDestination();
+  drawRoute();
 }
 
 // --Returns the current location of the user--
@@ -62,46 +65,73 @@ function getCurrent() {
     });
   }
 }
+function search(){
+  var pickup = document.getElementById('pickup');
+  var plbl= document.getElementById('p');
+  var psub= document.getElementById('psubmit');
 
+  var psearchBox = new google.maps.places.SearchBox(pickup);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(plbl);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(pickup);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(psub);
+
+  var dests = document.getElementById('dest');
+  var dlbl= document.getElementById('d');
+  var dsub= document.getElementById('dsubmit');
+
+  var dsearchBox = new google.maps.places.SearchBox(dests);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(dlbl);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(dests);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(dsub);
+
+
+}
 function setPickUp(){
-  ///document.getElementById(/*'Your ID'*/).addEventListener('click',function(){
-    var address='656 Hope Rd Kingston';
+  document.getElementById('psubmit').addEventListener('click',function(){
+    var address=$("#pickup").val();
+    // address=JSON.stringify(address);
+    console.log("this is: "+address)
     geocoder.geocode({'address': address},function(results,status){
       if (status === 'OK'){
-        pickUpLoc = results[0].geometry.location;
+        pickUpLoc = (results[0].geometry.location.lat()+','+results[0].geometry.location.lng());
+        console.log("Pick upif search "+pickUpLoc);
       }else{('We could not locate the address you entered. Status: '+status);}
-  //});
-  console.log("Pick up Loc if search "+pickUpLoc);
+
+  console.log("Pick up Loc if search "+/*JSON.stringify(*/pickUpLoc);
   // pickUp =new google.maps.Marker({
   //   position:pickUpLoc,
   //   title: 'Pick up',
   //   draggable:true,
   //   center: pickUpLoc
   // });
-  pickUp.setPosition(pickUpLoc);
+  pickUp.setPosition(results[0].geometry.location);
   pickUp.setMap(map);
   pickUp.setTitle("Pick up");
-  map.setCenter(pickUpLoc);
+  map.setCenter(results[0].geometry.location);
+});
 });
 }
 function setDestination(){
-  ///document.getElementById(/*'Your ID'*/).addEventListener('click',function(){
-    var address='121 Old Hope Road, Kingston';
+  document.getElementById('dsubmit').addEventListener('click',function(){
+    var address=$("#dest").val();
+    console.log("clicked but");
     geocoder.geocode({'address': address},function(results,status){
       if (status === 'OK'){
-        destLoc = results[0].geometry.location;
+        destLoc = (results[0].geometry.location.lat()+','+results[0].geometry.location.lng());
+        console.log("Dest if search "+destLoc);
       }else{('We could not locate the address you entered. Status: '+status);}
-  //});
+
   console.log("Destination "+destLoc);
   dest =new google.maps.Marker({
-    position:destLoc,
+    position:results[0].geometry.location,
     title: 'Destination',
     draggable:false
   });
   dest.setMap(map);
-  map.setCenter(destLoc);
-});
-// drawRoute(pickUpLoc,destLoc);
+  map.setCenter(results[0].geometry.location);
+  drawRoute();
+});});
+
 }
 
 // ----- Address search Listener---
@@ -144,14 +174,14 @@ function setDestination(){
 /*-----Draw route between two points-----
 @param: pickUp: pick up coordinates
         dest: destination coordinates*/
-function drawRoute(pickUpLoc,destLoc){
+function drawRoute(){
   var directionsService = new google.maps.DirectionsService;
   var directionsDisplay = new google.maps.DirectionsRenderer;
   directionsDisplay.setMap(map);
   //document.getElementById(/*Search/Request*/).addEventListener('click',function(){
     directionsService.route({
-    origin: '656 Hope Rd Kingston',//document.getElementById(/*pickUp*/).value,
-    destination: '121 Old Hope Road, Kingston',//document.getElementById(/*dest*/).value,
+    origin:$("#pickup").val(),//document.getElementById(/*pickUp*/).value,
+    destination: $("#dest").val(),//document.getElementById(/*dest*/).value,
     //waypoints: ['656 Hope Rd Kingston'],
     travelMode: 'DRIVING'
     },function(response,status){
